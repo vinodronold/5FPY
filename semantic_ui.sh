4e73aaaa14354f60dca45b0a55151f26d402071f
vinodronold <vinodronoldbe@gmail.com>
2016-09-08 22:53:05 -0400

    0808.3

--- semantic_ui.sh
+++ semantic_ui.sh
@@ -6,6 +6,6 @@ gulp build
 echo 'copying css to static . . .'
 cp dist/semantic.min.css ../../fivefrets/static/css/semantic.min.css
 echo 'copying js to static . . .'
-cp dist/semantic.min.js ../../fivefrets/static/css/semantic.min.js
+cp dist/semantic.min.js ../../fivefrets/static/js/semantic.min.js
 cd ../..
 echo 'sucessfully completed . . .'
