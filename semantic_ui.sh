69a33020f0e3ce0d9ec8ffdf193b16db7610de64
vinodronold <vinodronoldbe@gmail.com>
2016-10-02 20:31:48 -0400

    20161002.2

--- semantic_ui.sh
+++ semantic_ui.sh
@@ -8,6 +8,6 @@ cp dist/semantic.min.css ../../fivefrets/static/css/semantic.min.css
 echo 'copying js to static . . .'
 cp dist/semantic.min.js ../../fivefrets/static/js/semantic.min.js
 echo 'copying theme'
-cp -R dist/themes/ ../../fivefrets/static/css/themes/
+cp -R dist/themes/ ../../fivefrets/static/css/
 cd ../..
 echo 'sucessfully completed . . .'
