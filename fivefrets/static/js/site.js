66f8e6cf3d6d9ece33aea3a62789d19d226132b6
vinodronold <vinodronoldbe@gmail.com>
2016-10-10 23:48:12 -0400

    20161010.1

--- fivefrets/static/js/site.js
+++ fivefrets/static/js/site.js
@@ -21,34 +21,102 @@ $(document).ready(function(){
             'font-size': 16
         });
     }
-
-    Logo("fflogo");
-    $('#fflogo > svg').addClass("ui centered image");
-
-    Logo("ffsidebarlogo");
-    $('#ffsidebarlogo > svg').addClass("ui centered image");
-
+    $("div[id^='fflogo_']").each(function(index){
+      Logo($(this).attr('id'));
+    });
+    $("div[id^='fflogo_'] > svg").addClass("ui centered image");
 });
 $(document).ready(function(){
-    $.fn.search.settings.templates.youtube = function(response) {
-        var html = '';
-        html += '<div class="ui basic segment">';
-        html += '<div class="ui celled list">';
-        $.each(response.results, function(index, result) {
-            html += '<div class="item">';
-            html += '<div class="right floated content">';
-            html += '<div class="ui green mini button">Get Chords</div>';
-            html += '</div>';
-            html += '<img class="ui tiny image" src="'+result.image+'">';
-            html += '<div class="content">';
-            html += '<div class="header">'+result.title+'</div>';
-            //html += '<p>'+result.description+'</p>';
-            html += '</div>';
-            html += '</div>';
-        });
-        html += '</div>';
-        html += '</div>';
-        return html;
+    $("div[id^='ffcdia_']").each(function(index){
+        var startx = 15,
+            starty = 10,
+            stringSpace = 10,
+            fretSpace = 15,
+            strings = 6,
+            frets = 4,
+            startFretNum = $(this).attr('data-ffcdiastart'),
+            padding = 5,
+            //position = [1,0,0,3,3,3,0]; // [BARRE, STRING6, STRING5, STRING4, STRING3, STRING2, STRING1]
+            position = $(this).attr('data-ffcdia').split(","); // [BARRE, STRING6, STRING5, STRING4, STRING3, STRING2, STRING1]
+        var paperx = startx + (stringSpace * (strings - 1)) + padding,
+            papery = starty + (fretSpace * frets) + padding,
+            fretWidth = stringSpace * (strings - 1),
+            chordNamex = startx + ((stringSpace * (strings - 1)) / 2),
+            chordNamey = starty - padding,
+            stringPath,
+            fretPath;
+        stringPath = "";
+        for (cnt = 1; cnt < strings - 1; cnt++){
+            var stringStartx = (startx + (stringSpace * cnt));
+            stringPath = stringPath + "M"+ stringStartx + " " + starty + " " + "L" + stringStartx + " " + (starty + (fretSpace * frets));
+        }
+        fretPath = "";
+        for (cnt = 1; cnt < frets; cnt++){
+            var fretStarty = starty + (fretSpace * cnt);
+            fretPath = fretPath + "M" + startx + " " + fretStarty + "L" + (startx + fretWidth) + " " + fretStarty;
+        }
+        var cdia = new Raphael($(this).attr('id'), paperx, papery);
+        var cdia_frets = cdia.set();
+        cdia_frets.push(
+            cdia.text(chordNamex, chordNamey, $(this).attr('data-ffcdianame')),
+            cdia.rect(startx, starty, fretWidth, (fretSpace * frets), 2),
+            cdia.path(stringPath + fretPath)
+        );
+        for (cnt = 0; cnt < frets; cnt++){
+            cdia_frets.push(
+                cdia.text(padding, starty + ((fretSpace * cnt)+(fretSpace / 2)), (Number(startFretNum) + cnt))
+            );
+        }
+
+        var cdia_finger = cdia.set();
+        if (position[0] > 0 && position[0] <= frets){
+            var barreStarty = starty + (fretSpace * (position[0] - 1)) + (fretSpace / 3);
+            var barrePath = "M" + startx + " " + barreStarty
+            + "L" + (startx + fretWidth) + " " + barreStarty
+            + "A1 1 0 0 1 " + (startx + fretWidth) + " " + (barreStarty + 5)
+            + "L" + startx + " " + (barreStarty + 5)
+            + "A1 1 0 0 1 " + startx + " " + barreStarty;
+            cdia_finger.push(
+                cdia.path(barrePath)
+            );
+        }else{
+            console.log("Invalid Barre Positiion : " + position[0] + " Total Frets : " + frets);
+        }
+        $.each(position,function(index, value){
+            if (index > 0 && value > 0){
+                cdia_finger.push(
+                    cdia.circle((startx + (stringSpace * (index - 1))), (starty + (fretSpace * (value - 1)) + (fretSpace / 2)), 3)
+                );
+            }
+
+        })
+        cdia_finger.attr({stroke: "#333",'fill': '#333'});
+    });
+    $("div[id^='ffcdia_'] > svg").addClass("ui centered image");
+});
+$(document).ready(function(){
+    $.fn.embed.settings.sources = {
+        ff_youtube: {
+            name   : 'ff_youtube',
+            type   : 'video',
+            icon   : 'video play',
+            domain : 'youtube.com',
+            url    : '//www.youtube.com/embed/{id}',
+            parameters: function(settings) {
+                return {
+                    autohide       : !settings.showUI,
+                    autoplay       : settings.autoplay,
+                    color          : settings.colors || undefined,
+                    hq             : settings.hd,
+                    jsapi          : settings.api,
+                    modestbranding : 1,
+                    fs             : 0,
+                    rel            : 0,
+                    showinfo       : 0,
+                    playsinline    : 1
+                };
+            }
+        }
     };
     $('#ffsidebarmenu').click(function(){
         $('.ui.sidebar')
@@ -59,6 +127,20 @@ $(document).ready(function(){
     $('.special.cards .image').dimmer({
         on: 'hover'
     });
+
+    var pos = 0;
+    function scrollList(){
+        $(".chordsList").css("left", pos+"em");
+        pos = pos - 0.2;
+        // FOR 0.1em shift and 25ms - 1s is 4em
+        if (pos == -10) {
+            $(".chordsList").css("left", "0em");
+            clearInterval(timer);
+        }
+    }
+
+    var timer = setInterval(scrollList, 25);
+
     $('.ui.search')
         .search({
         //type          : 'youtube',
@@ -82,6 +164,5 @@ $(document).ready(function(){
             },
             url: 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q={query}&type=video&videoCategoryId=10&key=AIzaSyCpVWdjX0SBFRSfsYcxltR50IhOncXl0YU'
         }
-    })
-    ;
+    });
 });
