d0c6b6d9b06187b4a90a961ac070d5dd7ed67899
vinodronold <vinodronoldbe@gmail.com>
2016-10-24 22:42:47 -0400

    20161024.3

--- fivefrets/chords/estimate/features.py
+++ fivefrets/chords/estimate/features.py
@@ -1,4 +1,7 @@
+from __future__ import print_function
+import librosa
 import subprocess
+import csv
 
 class features:
 
@@ -6,6 +9,7 @@ class features:
         self.id = yt_id
         self.filepath = '/home/py/projects/tmp/'
         self.ext = 'mp3'
+        self.vamp_plugin = 'nnls-chroma_chordino_simplechord'
 
     def dowload(self):
         result = subprocess.check_call([
@@ -22,9 +26,24 @@ class features:
         result = subprocess.check_call([
             'sonic-annotator',
             '-d',
-            'vamp:nnls-chroma:chordino:simplechord',
+            'vamp:'+self.vamp_plugin,
             self.filepath + self.id + '.' + self.ext,
             '-w', 'csv',
             '--csv-force',
             '--force',
             ])
+
+    def convert_to_list(self):
+        with open(self.filepath + self.id + '_vamp_' + self.vamp_plugin + '.csv', 'r') as f:
+            reader = csv.reader(f)
+            chords = list(reader)
+
+        #for e in chords:
+        #    print(e)
+
+    def processing_beats(self):
+        audio, sample_rate = librosa.load(self.filepath + self.id + '.' + self.ext)
+        tempo, beat_frames = librosa.beat.beat_track(y=audio, sr=sample_rate)
+        print('Estimated tempo: {:.2f} beats per minute'.format(tempo))
+        beat_times = librosa.frames_to_time(beat_frames, sr=sample_rate)
+        librosa.output.times_csv(self.filepath + self.id + 'librosa_beat_times.csv', beat_times)
